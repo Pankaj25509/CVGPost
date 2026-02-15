d27cf99b9a0190463ec539792857bbe2f2c0372f
Pankaj25509 <pr25122005@gmail.com>
2026-02-15 14:00:22 +0530

    fixing vercel errors

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        booking: resolve(__dirname, 'booking.html'),
-      },
-    },
-  },
 })
