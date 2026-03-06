d6d247bc9c97bd287406202185b623556f0716cf
Pankaj25509 <pr25122005@gmail.com>
2026-03-06 19:43:58 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* When the link is just /, show the home page */}
         <Route path="/" element={<LandingPage />} />
-
-        {/* When the link is /booking, show the booking page */}
         <Route path="/booking" element={<BookingPage />} />
-        
-        {/* If the link is wrong, just go back home */}
-        <Route path="*" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
